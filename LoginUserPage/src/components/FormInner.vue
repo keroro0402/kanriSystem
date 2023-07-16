06b02d36797f4acd49a21c84eb7e63063a996833
keroro0402 <otsuyu0402@yahoo.co.jp>
2023-07-17 05:02:08 +0900

    first commit

---
+++ LoginUserPage/src/components/FormInner.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import { ref, reactive, onMounted } from 'vue'
+const formProperty = ref({
+  action: '/loginuser/create',
+  method: 'POST'
+})
+const title = ref('ログインユーザ登録')
+const idProperty = ref({
+  type: 'text',
+  id: 'loginUserId',
+  text: 'ID'
+})
+const pwProperty = ref({
+  type: 'text',
+  id: 'loginUserPw',
+  text: 'PW'
+})
+const buttonProperty = reactive({
+  common: {
+    type: 'submit',
+    name: 'submitButton'
+  },
+  registerButton: {
+    text: '登録',
+    value: 'register'
+  },
+  backButton: {
+    text: '戻る',
+    value: 'back'
+  }
+})
+
+onMounted((): void => {
+  firstFocus()
+})
+function firstFocus() {
+  const targetEl = document.getElementById('loginUserId')
+  targetEl.focus()
+}
+</script>
+<template>
+  <section>
+    <h1>{{ title }}</h1>
+    <form v-bind="formProperty">
+      <table>
+        <tr>
+          <td>
+            <label v-bind:for="idProperty.id">{{ idProperty.text }}</label
+            >：
+            <input v-bind:type="idProperty.type" v-bind:id="idProperty.id" />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <label v-bind:for="pwProperty.id">{{ pwProperty.text }}</label
+            >：
+            <input v-bind:type="pwProperty.type" v-bind:id="pwProperty.id" />
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <button
+              v-bind="buttonProperty.common"
+              v-bind:value="buttonProperty.registerButton.value"
+            >
+              {{ buttonProperty.registerButton.text }}
+            </button>
+            <button v-bind="buttonProperty.common" v-bind:value="buttonProperty.backButton.value">
+              {{ buttonProperty.backButton.text }}
+            </button>
+          </td>
+        </tr>
+      </table>
+    </form>
+  </section>
+</template>
+<style scoped>
+section,
+form {
+  text-align: center;
+}
+h1 {
+  font-size: 1.5rem;
+  padding: 1rem;
+}
+table {
+  margin: 10px auto 30px auto;
+  border: 1px solid black;
+  width: 80%;
+  max-width: 400px;
+}
+table tr {
+  height: 4rem;
+}
+table tr td button {
+  margin: 2rem;
+  width: 5rem;
+}
+</style>
---
+++ LoginUserPage/src/components/Header.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+
+const HeaderTitle = reactive({
+  siteTitle: 'Hiyokko',
+  icon: './src/assets/Piyoko.png'
+})
+</script>
+<template>
+  <header>
+    <h1><img v-bind:src="HeaderTitle.icon" />{{ HeaderTitle.siteTitle }}</h1>
+  </header>
+</template>
+<style scoped>
+header {
+  color: rgb(255, 255, 255, 255);
+  background: rgb(128, 0, 0, 255);
+  padding: 0.5rem 0;
+}
+header h1 {
+  text-align: center;
+}
+header h1 img {
+  vertical-align: middle;
+  width: 1em;
+  padding: 0 0.5em 0 0;
+}
+</style>
