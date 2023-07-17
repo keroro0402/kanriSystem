aec917811fa2712d9ccfa4b4f34842798cae4845
keroro0402 <otsuyu0402@yahoo.co.jp>
2023-07-18 06:19:00 +0900

    ドキュメントルートへのgetリクエストを追記した サーバアクセス時のコンソール出力文字を変更した

--- LoginUserPage/index.js
+++ LoginUserPage/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('こんにちは！このメッセージが見えますか？？？');
+});
+
 app.listen(5100, () => {
-  console.log('Listening on localhost port 5100');
+  console.log('5100番で準備が出来ました。5100にアクセスしてみてください。');
 });
