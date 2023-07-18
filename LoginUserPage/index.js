9ab37e4bc36f5d736804bf2d43805d5251359dd1
keroro0402 <otsuyu0402@yahoo.co.jp>
2023-07-19 08:02:27 +0900

    /loginからのpostリクエストを追記しました

--- LoginUserPage/index.js
+++ LoginUserPage/index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
   res.send('こんにちは！このメッセージが見えますか？？？');
 });
 
+app.post('/login', (req, res) => {
+  console.log('POSTリクエストされました');
+});
+
 app.listen(5100, () => {
   console.log('5100番で準備が出来ました。5100にアクセスしてみてください。');
 });
