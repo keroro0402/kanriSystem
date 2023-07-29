d75fb2eda4b27044e50645ad64257f7f707a1834
koshi0160 <monstak.14756@gmail.com>
2023-07-29 15:43:29 +0900

    引き継ぎ

---
+++ LoginUserPage/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Header from '@/components/Header.vue';
+import FormInner from '@/components/FormInner.vue';
+import Footer from '@/components/Footer.vue';
+</script>
+<template>
+  <Header></Header>
+  <FormInner></FormInner>
+  <Footer></Footer>
+</template>
---
+++ LoginUserPage/src/components/Header.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import img from '@/assets/Piyoko.png';
+
+const HeaderTitle = reactive({
+  siteTitle: 'Hiyokko',
+  icon: img,
+});
+</script>
+<template>
+  <header>
+    <h1><img v-bind:src="HeaderTitle.icon" />{{ HeaderTitle.siteTitle }}</h1>
+  </header>
+</template>
+<style scoped>
+header {
+  color: rgb(255, 255, 255, 255);
+  background: rgb(128, 0, 0, 255);
+  padding: 0.5rem 0;
+}
+header h1 {
+  text-align: center;
+}
+header h1 img {
+  vertical-align: middle;
+  width: 1em;
+  padding: 0 0.5em 0 0;
+}
+</style>
